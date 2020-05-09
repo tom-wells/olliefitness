fef2dfec14a6a6fd7ee327821dbd51b2c1943a60
Tom Wells <tom.wells89@gmail.com>
2020-05-09 12:49:41 +0100

    added mobile button animation

--- assets/scripts/mobile.js
+++ assets/scripts/mobile.js
@@ -1,8 +1,13 @@
-const menuButton = document.querySelector(".menu-button")
 const navMenu = document.querySelector(".mobile-nav")
+const buttonContainer = document.querySelector(".button-container")
 
-menuButton.addEventListener("click", function() {
-    navMenu.classList.toggle("open")
+buttonContainer.addEventListener("click", function() {
+    buttonContainer.classList.toggle("active");
+    if (buttonContainer.classList.contains("active")) {
+        navMenu.classList.add("open")
+    } else {
+        navMenu.classList.remove("open")
+    }
 });
 
 
